6c64ff886f2616a174333baef4ba96dd55b5488b
Drew <drew@scurvynet.com>
2017-02-19 23:04:10 +0000

    finished wrapper, will not wrap, but pass new line will work

--- bin/lcdWrapper.py
+++ bin/lcdWrapper.py
@@ -6,10 +6,13 @@ from subprocess import *
 from time import sleep, strftime
 from datetime import datetime
 
+# LCD settings
+DISPLAY_ROWS = 2
+DISPLAY_COLS = 16
 lcd = Adafruit_CharLCD()
+lcd.begin(DISPLAY_COLS, DISPLAY_ROWS)
 
-inMessage = sys.argv[1:]
-
-lcd.begin(16, 1)
+inMessage = sys.argv[1].decode("string_escape")
 lcd.clear()
-lcd.message(inMessage)
+lcd.message(str(inMessage))
+
