95901bef52093634e250e5bed1d53636d4276816
Drew <drew@scurvynet.com>
2017-02-20 06:34:27 +0000

    adding missing module

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@
 
 wget http://raspberry.znix.com/hipifiles/hipi-install
 perl hipi-install
-cpanm Mojolicious::Lite LWP::Simple Log::Log4perl 
+cpanm Mojolicious::Lite LWP::Simple Log::Log4perl Date::Calc
