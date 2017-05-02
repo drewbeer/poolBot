06a0de25e4551085f12ca3a9a220396bf5124b2d
Drew Matthews <drew@scurvynet.com>
2017-05-01 23:49:55 -0700

    fixed rachio curl

--- bin/poolBot.pl
+++ bin/poolBot.pl
@@ -91,7 +91,9 @@ sub fetchUrl {
 # fetch the rachio url
 sub fetchRachioUrl {
   my ($url, $value) = @_;
-  my $curlUrl = "curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Bearer 'rachioKey' -d '$value' $url";
+  my $curlUrl = qq (
+    curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Bearer 'rachioKey' -d "$value" $url
+  );
   my $response = `$curlUrl`;
   if (!$response) {
     return 0;
