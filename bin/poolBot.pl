7a181b6325690c365ba2af522305731c816fdf80
Drew Matthews <drew@scurvynet.com>
2017-09-14 23:01:15 -0700

    node-red replaced everything almost, so i ripped out all the code i don't need naymore

--- bin/poolBot.pl
+++ bin/poolBot.pl
@@ -1,32 +1,18 @@
 #!/usr/bin/perl
-# poolBot, a perl based web api to talk to different things that affect my pool
-#
-## TODO:
-# OMG OMG OMG https://metacpan.org/pod/OAuth::Cmdline::Smartthings
-# health check (salt cannot run without the pump running) should be thread
-# scheduling
-# slack output
-# web status in json
-# settings page
-#
-#
+# poolBot, a simple perl bot that runs a health check on the pool pump, and controls the relays
+
 use strict;
 use warnings;
 use Mojolicious::Lite;
 use Mojo::JSON qw(decode_json encode_json);
 use Mojo::Redis2;
-use RocksDB;
 use Try::Tiny;
-use DateTime;
-use Schedule::Cron;
 use Data::Dumper;
-use Net::Prometheus;
 
-# debug
+# debug log and
 app->log->level('debug');
 
 my $pumpUrl = "http://10.42.2.19:3000";
-my $rachioKey = "4236ff47-df71-4c5d-8520-c4fa9236944d";
 my $listenWebPort = 'http://*:3000';
 my $dbLocation = '../etc/poolBot.db';
 
@@ -41,124 +27,26 @@ $relays->{'salt'} = 16;
 $relays->{'heater'} = 12;
 $relays->{'spa'} = 18;
 
-# # Check title in the background every 10 seconds
-# my $title = 'Got no title yet.';
-# Mojo::IOLoop->recurring(10 => sub {
-#   app->ua->get('http://mojolicious.org' => sub {
-#     my ($ua, $tx) = @_;
-#     $title = $tx->result->dom->at('title')->text;
-#   });
-# });
-
+## declares
 my $poolBot = ();
-my $cron="";
-my $db="";
 
-# setup the db
+# setup redis handler globally
 my $redis = Mojo::Redis2->new;
 $redis->set(term => "0");
 
+### FUNCTIONS ###
 # Startup function
 sub startup {
   my $self = shift;
   app->log->info('poolBot Starting Up');
 
-  # clear the rocksDB term status
-
   # GPIO setup
   # make sure all pins are set to low
-  app->log->info('Setting replay pins');
+  app->log->info('setting all relays to off');
   foreach my $pin (keys %{ $relays }) {
     `$gpioCMD export $relays->{$pin} low`;
   }
-
-  # lets pull the default schedule for cron
-  app->log->fino('Retrieving stored crontab');
-  my $cronJSON = $self->db->get('crontab');
-
-  if ($cronJSON)  {
-    my $crontab = decode_json $cronJSON;
-    # load the cron
-    foreach my $cron ( %{ $crontab }) {
-      # $self->cron->add_entry($cron->{'datetime'},\&cronScheduler,$self,$cron->{'mode'},$cron->{'duration'});
-    }
-    # run the cron and detach
-    # $self->cron->run(detach=>1,pid_file=>"../log/scheduler.pid");
-  }
-
-  # now we should startup any threads for background stuff
-
 }
-
-# cron details, aka modes
-# when generating cron, it should have two entries.
-# 1. general crontab entry, that is when, and what and how long to run something for
-# 2. details about the specific cron to run
-#   a. pump only, pump and salt, pump and heater, so on.
-#   b. how fast the pump should run
-#   c. how high to let the temp get to on heat functions
-#
-# modes should have a key prefix, like
-# 1. mode_pump_only
-# 2. mode_heat
-# 3. mode_normal_high
-# 4. mode_normal_med
-# 5. mode_normal_low
-sub cronScheduler {
-  my $args = shift;
-  my $self = $args->{self};
-  my $mode = $args->{mode};
-  my $duration = $args->{duration};
-
-  # lets get the specific details of this program
-  my $cronModes = $self->db->get($mode);
-
-}
-
-# stats to prometheus
-sub statsFork {
-  app->log->info('statsFork: Starting Stats fork to prometheus');
-  my $prometheus = Net::Prometheus->new;
-  my $pbMetrics = $prometheus->new_metricgroup( namespace => "poolBot" );
-
-  ## we need to setup the different types of metrics
-  ## Pump Speed
-  my $pumpRPM = $pbMetrics->new_gauge(
-     name => "rpms",
-     help => "Current rpms of motor",
-  );
-  ## Pump watts
-  my $pumpWatt = $pbMetrics->new_gauge(
-     name => "watts",
-     help => "Current watts of motor",
-  );
-  ## Pump duration
-  my $pumpDuration = $pbMetrics->new_gauge(
-     name => "duration",
-     help => "time left for running pump",
-  );
-
-  while (!$redis->get("term")) {
-    # fetch the systemStatus and parse it
-    my $systemStatus = systemStatus();
-
-    # set pump
-    $pumpRPM->set( $systemStatus->{'pump'}->{'rpm'} );
-
-    # set pump
-    $pumpWatt->set( $systemStatus->{'pump'}->{'watts'} );
-
-    # set pump
-    $pumpDuration->set( $systemStatus->{'pump'}->{'currentrunning'}->{'remainingduration'} );
-
-    # lets update redis so that it can be scraped
-    my $promOutput = $prometheus->render;
-    $redis->set(stats => $promOutput);
-    sleep 7;
-  }
-  exit;
-}
-
 
 # monitor fork for handling the health check and such
 sub monFork {
@@ -215,18 +103,67 @@ sub monFork {
   return;
 }
 
-# generate timestamp data
-sub timeStamp {
-	my $self = shift;
-	# returns a timestamp for the file
-	my $timestamp = ();
-	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
-	$timestamp->{'file'} = sprintf ( "%04d%02d%02d-%02d.%02d.%02d", $year+1900,$mon+1,$mday,$hour,$min,$sec);
-	$timestamp->{'now'} = sprintf ( "%04d%02d%02d %02d:%02d:%02d", $year+1900,$mon+1,$mday,$hour,$min,$sec);
-	$timestamp->{'nowMinute'} = sprintf ( "%02d", $min);
+## relay control ##
+# toggle relays
+sub relayControl {
+  my ($relay, $value) = @_;
+  if (!$relay || !$value) {
+    return 0;
+  }
+  my $relayStatus;
 
-	return $timestamp;
-};
+  # before we allow toggles we should have some limits
+  my $systemStatus = systemStatus();
+  if (($systemStatus->{'pump'}->{'currentrunning'}->{'mode'} eq 'off') && ($value eq 'on')) {
+    if ($relay eq 'heater' || $relay eq 'salt') {
+      app->log->warn("can't enable $relay, pump is $systemStatus->{'pump'}->{'currentrunning'}->{'mode'}");
+      return 0;
+    }
+  }
+
+  # write the gpio value using a shell
+  if ($value eq 'on') {
+    `$gpioCMD write $relays->{$relay} 1`;
+    $relayStatus = relayStatus($relay);
+  } elsif ($value eq 'off') {
+    `/usr/bin/gpio -g write $relays->{$relay} 0`;
+    $relayStatus = relayStatus($relay);
+  }
+  return $relayStatus;
+}
+
+# get the relay status
+sub getRelayStatus {
+  my ($relay) = @_;
+  if (!$relay) {
+    return 0;
+  }
+  my $relayStatusPretty = "off";
+  # get relay status
+  my $relayStatus = `$gpioCMD read $relays->{$relay}`;
+  chomp $relayStatus;
+
+  # if the relay is true then its "on"
+  if ($relayStatus) {
+    $relayStatusPretty = "on";
+  }
+  return $relayStatusPretty;
+}
+
+## utility code
+# fetch the system status from redis and decode it
+sub systemStatus {
+  my $lastStatus = $redis->get("systemStatus");
+  my $systemStatus = decode_json $lastStatus;
+  return $systemStatus;
+}
+
+sub terminate {
+  # turn off all the relays
+  foreach my $pin (keys %{ $relays }) {
+    `$gpioCMD write $relays->{$pin} 0`;
+  }
+}
 
 # pass url, and if json should be parsed
 sub fetchUrl {
@@ -261,128 +198,7 @@ sub fetchUrl {
 
   return 0;
 };
-
-# fetch the rachio url
-sub fetchRachioUrl {
-  my ($url, $value) = @_;
-  my $curlUrl = "curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Bearer 'rachioKey' -d '$value' $url";
-  my $response = `$curlUrl`;
-  if (!$response) {
-    return 0;
-  }
-  my $decodedResponse = decode_json($response);
-  return $decodedResponse;
-};
-
-sub relayControl {
-  my ($relay, $value) = @_;
-  if (!$relay || !$value) {
-    return 0;
-  }
-  my $relayStatus;
-
-  # before we allow toggles we should have some limits
-  my $systemStatus = systemStatus();
-  if (($systemStatus->{'pump'}->{'currentrunning'}->{'mode'} eq 'off') && ($value eq 'on')) {
-    if ($relay eq 'heater' || $relay eq 'salt') {
-      app->log->warn("can't enable $relay, pump is $systemStatus->{'pump'}->{'currentrunning'}->{'mode'}");
-      return 0;
-    }
-  }
-
-  # write the gpio value using a shell
-  if ($value eq 'on') {
-    `$gpioCMD write $relays->{$relay} 1`;
-    $relayStatus = relayStatus($relay);
-  } elsif ($value eq 'off') {
-    `/usr/bin/gpio -g write $relays->{$relay} 0`;
-    $relayStatus = relayStatus($relay);
-  }
-  return $relayStatus;
-}
-
-sub relayStatus {
-  my ($relay) = @_;
-  if (!$relay) {
-    return 0;
-  }
-  my $relayStatusPretty = "off";
-  # get relay status
-  my $relayStatus = `$gpioCMD read $relays->{$relay}`;
-  chomp $relayStatus;
-
-  # if the relay is true then its "on"
-  if ($relayStatus) {
-    $relayStatusPretty = "on";
-  }
-  return $relayStatusPretty;
-}
-
-# fetch the key from redis and decode it
-sub systemStatus {
-  my $lastStatus = $redis->get("systemStatus");
-  my $systemStatus = decode_json $lastStatus;
-  return $systemStatus;
-}
-
-sub terminate {
-  # turn off all the relays
-  foreach my $pin (keys %{ $relays }) {
-    `$gpioCMD write $relays->{$pin} 0`;
-  }
-}
-# Create db connection if needed
-helper db => sub {
-    if ($db) {
-      return $db;
-    } else {
-      $db = RocksDB->new($dbLocation, { create_if_missing => 1 });
-      return $db;
-    }
-};
-
-# cron
-helper cron => sub {
-  if ($cron) {
-    return $cron;
-  } else {
-    $cron = new Schedule::Cron(  sub { print "@_","\n" },
-                                  file  => "../etc/poolBot.sched",
-                                  eval  => 1);
-    return $cron;
-  }
-};
-
-# rachio helper
-#
-# start zone 8
-# value: '{ "id" : "c1ec26b1-f514-44d1-bcec-bf46c7bea5c8", "duration" : 60 }'
-# url: 'https://api.rach.io/1/public/zone/start'
-#
-helper startPoolFill => sub {
-  my ($self, $duration) = @_;
-  my $rachioValue = "{ 'id' : 'c1ec26b1-f514-44d1-bcec-bf46c7bea5c8', 'duration' : $duration }";
-  my $rachioStartUrl = 'https://api.rach.io/1/public/zone/start';
-  my $rachioResponse = fetchRachioUrl($rachioStartUrl, $rachioStartUrl);
-  if ($rachioResponse) {
-    print Dumper($rachioResponse);
-  }
-  return $rachioResponse;
-};
-
-# # stop all water
-# value '{ "id" : "72c57cc8-ce7e-4faa-a99a-1740aa1a2431" }'
-# url 'https://api.rach.io/1/public/device/stop_water'
-helper stopPoolFill => sub {
-  my ($self) = @_;
-  my $rachioValue = '{ "id" : "72c57cc8-ce7e-4faa-a99a-1740aa1a2431" }';
-  my $rachioStopUrl = 'https://api.rach.io/1/public/device/stop_water';
-  my $rachioResponse = fetchRachioUrl($rachioStopUrl, $rachioValue);
-  if ($rachioResponse) {
-    print Dumper($rachioResponse);
-  }
-  return $rachioResponse;
-};
+### end of functions ###
 
 # pump status
 helper fetchPumpStatus => sub {
@@ -391,31 +207,6 @@ helper fetchPumpStatus => sub {
   return $systemStatus->{'pump'};
 };
 
-# pump run
-helper setPumpRun => sub {
-  my ($self, $pumpID, $program, $duration) = @_;
-  # fetch the pump status and only one since thats all we have
-  my $pumpRunCMD = "$pumpUrl/pumpCommand/run/pump/$pumpID/program/$program/duration/$duration";
-  my $pumpResponse = fetchUrl($pumpRunCMD,1);
-  return $pumpResponse;
-};
-
-# pump power
-helper setPumpPower => sub {
-  my ($self, $pumpID, $value) = @_;
-  my $pumpRunCMD = "$pumpUrl/pumpCommand/$value/pump/$pumpID";
-  my $pumpResponse = fetchUrl($pumpRunCMD,1);
-  return $pumpResponse;
-};
-
-# pump status
-helper setPumpProgram => sub {
-  my ($self, $pumpID, $program, $rpm) = @_;
-  # fetch the pump status and only one since thats all we have
-  my $pumpProgramCMD = "$pumpUrl/pumpCommand/save/pump/$pumpID/program/$program/rpm/$rpm";
-  my $pumpResponse = fetchUrl($pumpProgramCMD,1);
-  return $pumpResponse;
-};
 
 # relay control
 helper toggleRelay => sub {
@@ -427,38 +218,9 @@ helper toggleRelay => sub {
 # relay status
 helper relayStatus => sub {
   my ($self, $relay) = @_;
-  my $relayStatus = relayStatus($relay);
+  my $relayStatus = getRelayStatus($relay);
   return $relayStatus ;
 };
-
-
-## Api Routes
-# # Always check auth token!  Here we validate that every API request
-# # has a valid token
-# under sub {
-#     my $self  = shift;
-#     my $token   = $self->param('token');
-#
-#     if (!$token) {
-#       $self->render(text => "Access Token Required, goodbye.");
-#       $self->db_disconnect;
-#       return;
-#     }
-#
-#     my $SQL = "select name from authTokens where token = '$token' and active = 1";
-#     my $cursor = $self->db->prepare($SQL);
-#     $cursor->execute;
-#     my @name = $cursor->fetchrow;
-#     $cursor->finish;
-#
-#     if($name[0]){
-#         return 1;
-#     }else{
-#         $self->render(text => "I'm sorry, Dave. I'm afraid I can't do that.");
-#         $self->db_disconnect;
-#         return;
-#     }
-# };
 
 # monitoring fork
 my $monFork = fork();
@@ -467,117 +229,12 @@ if ($monFork) {
   monFork();
 }
 
-# stats fork
-my $statsFork = fork();
-
-if ($statsFork) {
-  statsFork();
-}
-
 # webFork
 my $webFork = fork();
 
 # web fork module
 if ($webFork) {
-  # Power from rainforest
-  get '/api/power/incoming' => sub {
-      my $self  = shift;
-      my $pumpID  = $self->stash('id');
-      if (!$pumpID) {
-        return $self->render(json => {error => "missing pump number"});
-      }
-      my $pumpStatus = $self->fetchPumpStatus($pumpID);
-      if (!$pumpStatus) {
-        return $self->render(json => {error => "pump controller unavailable"});
-      }
-      return $self->render(json => {name => $pumpStatus->{$pumpID}->{'name'}, watts => $pumpStatus->{$pumpID}->{'watts'}, rpm => $pumpStatus->{$pumpID}->{'rpm'}, run => $pumpStatus->{$pumpID}->{'run'}, program1rpm => $pumpStatus->{$pumpID}->{'program1rpm'}, program1rpm => $pumpStatus->{$pumpID}->{'program1rpm'}, program2rpm => $pumpStatus->{$pumpID}->{'program2rpm'}, program3rpm => $pumpStatus->{$pumpID}->{'program3rpm'}, program4rpm => $pumpStatus->{$pumpID}->{'program4rpm'},programRemaining => $pumpStatus->{$pumpID}->{'currentrunning'}->{'remainingduration'}, programRunning => $pumpStatus->{$pumpID}->{'currentrunning'}->{'value'}, programMode => $pumpStatus->{$pumpID}->{'currentrunning'}->{'mode'}});
-  };
-
-  # pump status
-  get '/api/pump/status/:id' => sub {
-      my $self  = shift;
-      my $pumpID  = $self->stash('id');
-      if (!$pumpID) {
-        return $self->render(json => {error => "missing pump number"});
-      }
-      my $pumpStatus = $self->fetchPumpStatus($pumpID);
-      if (!$pumpStatus) {
-        return $self->render(json => {error => "pump controller unavailable"});
-      }
-      return $self->render(json => {name => $pumpStatus->{$pumpID}->{'name'}, watts => $pumpStatus->{$pumpID}->{'watts'}, rpm => $pumpStatus->{$pumpID}->{'rpm'}, run => $pumpStatus->{$pumpID}->{'run'}, program1rpm => $pumpStatus->{$pumpID}->{'program1rpm'}, program1rpm => $pumpStatus->{$pumpID}->{'program1rpm'}, program2rpm => $pumpStatus->{$pumpID}->{'program2rpm'}, program3rpm => $pumpStatus->{$pumpID}->{'program3rpm'}, program4rpm => $pumpStatus->{$pumpID}->{'program4rpm'},programRemaining => $pumpStatus->{$pumpID}->{'currentrunning'}->{'remainingduration'}, programRunning => $pumpStatus->{$pumpID}->{'currentrunning'}->{'value'}, programMode => $pumpStatus->{$pumpID}->{'currentrunning'}->{'mode'}});
-  };
-
-  # set the pump program.
-  get '/api/pump/set/:id/:program/:rpm' => sub {
-      my $self  = shift;
-      my $pumpID  = $self->stash('id');
-      my $program  = $self->stash('program');
-      my $rpm  = $self->stash('rpm');
-      if (!$rpm && !$program && !$pumpID) {
-        return $self->render(json => {error => "missing fields"});
-      }
-      my $pumpResponse = $self->setPumpProgram($pumpID, $program, $rpm);
-      if (!$pumpResponse) {
-        return $self->render(json => {error => "pump controller unavailable"});
-      }
-      return $self->render(json => {pump => $pumpResponse->{'pump'}, program => $pumpResponse->{'program'}, rpm => $pumpResponse->{'speed'}});
-  };
-
-  # set the pump run with duration
-  get '/api/pump/run/:id/:program/:duration' => sub {
-      my $self  = shift;
-      my $pumpID  = $self->stash('id');
-      my $program  = $self->stash('program');
-      my $duration  = $self->stash('duration');
-      if (!$duration && !$program && !$pumpID) {
-        return $self->render(json => {error => "missing fields"});
-      }
-      my $pumpResponse = $self->setPumpRun($pumpID, $program, $duration);
-      if (!$pumpResponse) {
-        return $self->render(json => {error => "pump controller unavailable"});
-      }
-      return $self->render(json => {pump => $pumpResponse->{'pump'}, program => $pumpResponse->{'program'}, duration => $pumpResponse->{'duration'}});
-  };
-
-  # turn on pool fill
-  get '/api/pool/water/on/:duration' => sub {
-      my $self  = shift;
-      my $duration  = $self->stash('duration');
-      if (!$duration) {
-        return $self->render(json => {error => "missing duration"});
-      }
-      my $poolFillResponse = $self->startPoolFill($duration);
-      if (!$poolFillResponse) {
-        return $self->render(json => {error => "rachio not working"});
-      }
-      return $self->render(json => {pump => $poolFillResponse->{'duration'}, power => $poolFillResponse->{'remaining'}});
-  };
-
-  # turn off pool fill
-  get '/api/pool/water/off' => sub {
-      my $self  = shift;
-      my $poolFillResponse = $self->stopPoolFill();
-      if (!$poolFillResponse) {
-        return $self->render(json => {error => "rachio not working"});
-      }
-      return $self->render(json => {pump => $poolFillResponse->{'status'}});
-  };
-
-  # pump on or off
-  get '/api/pump/power/:id/:value' => sub {
-      my $self  = shift;
-      my $pumpID  = $self->stash('id');
-      my $value  = $self->stash('value');
-      if (!$pumpID && !$value) {
-        return $self->render(json => {error => "missing fields"});
-      }
-      my $pumpResponse = $self->setPumpPower($pumpID, $value);
-      if (!$pumpResponse) {
-        return $self->render(json => {error => "pump controller unavailable"});
-      }
-      return $self->render(json => {pump => $pumpResponse->{'pump'}, power => $pumpResponse->{'power'}});
-  };
-
+  ## relay API code ##
   # relay control
   get '/api/relay/set/:name/:value' => sub {
       my $self  = shift;
@@ -610,13 +267,6 @@ if ($webFork) {
     my $loop = Mojo::IOLoop->singleton;
     $loop->timer( 1 => sub { terminate(); exit } );
     $loop->start unless $loop->is_running; # portability
-  };
-
-  # metrics for prometheus
-  get '/metrics' => sub {
-    my $self = shift;
-    my $statsData = $redis->get('stats');
-    return $self->render(text => $statsData);
   };
 
   # Start the app
